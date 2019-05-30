73e5b84a0c398f11d65482ab4d944323adadb8fb
n1nka <sakaev@it-projects.info>
2019-05-30 15:13:46 +0500

    unicode to string for python versions

--- pylint_odoo/checkers/itp_checks.py
+++ pylint_odoo/checkers/itp_checks.py
@@ -146,7 +146,11 @@ class ITPModuleChecker(misc.WrapperModuleChecker):
             if version != '':
                 manifest_odoo_version = '.'.join(version.split('.')[:2])
                 valid_odoo_version = self.linter._all_options[
-                    'valid_odoo_versions'].config.valid_odoo_versions[0].encode('utf-8')
+                'valid_odoo_versions'].config.valid_odoo_versions[0]
+                if sys.version_info[0] == 2:
+                    valid_odoo_version = valid_odoo_version.encode('utf-8')
+                else:
+                    valid_odoo_version = str(valid_odoo_version, 'utf-8')
                 if manifest_odoo_version != valid_odoo_version and installable:
                     print("Odoo version from ___manifest__.py is %s" % (manifest_odoo_version))
                     print("Expected odoo version is %s" % (valid_odoo_version))
